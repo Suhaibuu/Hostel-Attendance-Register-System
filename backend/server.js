16a20d21bc713e16f45758616fd9404ce7d93d78
suhaibuu <muhsuhaib1122@gmail.com>
2026-06-30 16:59:42 +0530

    added new domain

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://127.0.0.1:5173',
   'https://hostel-attendance-register-system.vercel.app'
+  'https://menshostel.vercel.app'
 ];
 
 app.use(
